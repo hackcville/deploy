81b39887b44b7f27c2b3dcafc18a6bc5da5f5452
Chris Santamaria <chris@santamaria.me>
2020-01-24 15:02:59 -0500

    Implement /api/post endpoint

--- week1/dwitter/dweets/urls.py
+++ week1/dwitter/dweets/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index)
+    path('', views.index),
+    path('api/post', views.post)
 ]
--- week1/dwitter/dweets/views.py
+++ week1/dwitter/dweets/views.py
@@ -1,7 +1,55 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+import json
+from jsonschema import validate
+from django.utils import timezone
+
 from .models import Dweet, User
 
 
 def index(request):
     dweets = Dweet.objects.order_by('date')
     return render(request, 'index.html', {'dweets': dweets})
+
+
+schema_post = {
+    'type': 'object',
+    'properties': {
+        'user': {
+            'type': 'string'
+        },
+        'message': {
+            'type': 'string'
+        },
+    }
+}
+
+
+@csrf_exempt
+def post(request):
+    if request.method == 'POST':
+        body = json.loads(request.body.decode('utf-8'))
+
+        try:
+            validate(instance=body, schema=schema_post)
+        except Exception as e:
+            return JsonResponse({
+                'success': False,
+                'message': 'Invalid request body!',
+                'error': str(e)
+            })
+
+        try:
+            user = User.objects.get(name=body['user'])
+        except:
+            user = User(name=body['user'])
+            user.save()
+
+        dweet = Dweet(user=user, message=body['message'], date=timezone.now())
+        dweet.save()
+
+        return JsonResponse({
+            'success': True,
+            'message': 'Succesfully posted the dweet!'
+        })
