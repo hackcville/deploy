547f455c8a9fb6fd63f747f1704e1463f36680f5
Chris Santamaria <chris@santamaria.me>
2020-01-24 14:44:47 -0500

    Remove unused API urls

--- week1/dwitter/dweets/urls.py
+++ week1/dwitter/dweets/urls.py
@@ -3,7 +3,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index),
-    # path('/api/dweets', views.dweets),
-    path('api/post', views.post)
+    path('', views.index)
 ]
--- week1/dwitter/dweets/views.py
+++ week1/dwitter/dweets/views.py
@@ -1,10 +1,7 @@
 from django.shortcuts import render
-
 from .models import Dweet, User
 
 
 def index(request):
     dweets = Dweet.objects.order_by('date')
-    print(dweets)
-
     return render(request, 'index.html', {'dweets': dweets})
